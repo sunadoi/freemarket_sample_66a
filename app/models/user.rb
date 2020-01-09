0e48007736fd3f368489fa505513615e2386a28e
sunadoi <y.sunadoi@gmail.com>
2020-01-10 00:11:23 +0900

    Create users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :likes
+  has_many :comments
+  has_one :address
+  has_one :card
+  has_one :buyer
+  has_one :seller
+
+  validates :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthdate, presence: true
+  validates :nickname, :email, :tel, presence: true, uniqueness: true
 end
