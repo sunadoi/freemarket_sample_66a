1f39f2bec9dee50074d0025ae06055f9c0a3a2d7
ogaryou <57928285+ogaryou@users.noreply.github.com>
2020-01-15 13:05:41 +0900

    controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,25 @@ class ProductsController < ApplicationController
   def new
   end
   
+  def edit
+    @product= Product.find(params[:id])
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      redirect_to root_path, notice: '編集できませんでした'
+    end  
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :description, :shipping_charge, :shipping_method, :shipping_prefecture, :shipping_days, :price)
+  end
+
+  
+
 end
