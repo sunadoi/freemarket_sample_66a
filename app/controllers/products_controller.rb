27f6e926bdce08da455086bd3ac41a24dc4402d1
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2020-01-15 16:54:26 +0900

    get values from db

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,12 @@ class ProductsController < ApplicationController
   end
   
   def show
-    @product = Product.includes([:photos, :brand, :category]).find(params[:id])
-    @products = Product.all
+    @product = Product.includes([:photos, :brand, :category, :seller]).find(params[:id])
+    @seller_products = @product.seller.user.products.limit(6)
+    eval = @product.seller.user.sellers.map{ |e| e[:evaluate] }
+    @good = eval.count(1)
+    @ok = eval.count(2)
+    @bad = eval.count(3)
   end
 
 end
