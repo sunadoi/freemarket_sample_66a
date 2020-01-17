5c41b2f15b4de31731115c98a9ba5487ac3a9a99
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2020-01-17 21:24:36 +0900

    Update products_controller.rb
    refactor sellers evaluate

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   before_action :set_product, except: [:new, :index]
-  include ApplicationHelper
 
   def index
       @ladys = Product.where(category_id:1..190).first(10)
@@ -18,11 +17,10 @@ class ProductsController < ApplicationController
   end
   
   def show
-    @seller_products = @product.seller.user.products.limit(6)
-    eval = @product.seller.user.sellers.map{ |e| e[:evaluate] }
-    @good = eval.count(1)
-    @ok = eval.count(2)
-    @bad = eval.count(3)
+    @seller_products = @product.seller.user.products.order(id: "DESC").first(6)
+    @good = Seller.where(user_id: @product.seller.user.id, evaluate: 1).count
+    @ok = Seller.where(user_id: @product.seller.user.id, evaluate: 2).count
+    @bad = Seller.where(user_id: @product.seller.user.id, evaluate: 3).count
   end
 
   def destroy
