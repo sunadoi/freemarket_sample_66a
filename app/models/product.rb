ea64e0dc3e127782a2891f905d7ad63e62272324
oono0908 <damian_wkids@yahoo.co.jp>
2020-01-20 19:33:08 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,8 +11,7 @@ class Product < ApplicationRecord
   has_many :likes
   has_many :comments
   has_one :buyer
-  has_one :seller, dependent: :destroy
-
+  has_one :seller, dependent: :destroy 
   validates :name, :description, :shipping_charge, :shipping_method, :shipping_prefecture, :shipping_days, :price, :progress, presence: true
 
   enum size: {
