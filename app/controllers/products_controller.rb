4141f73bb9efff7355899eded62410c6ac9576e4
yugo333 <yugo199103@gmail.com>
2020-01-22 21:33:52 +0900

    update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,15 +4,15 @@ class ProductsController < ApplicationController
   before_action :set_ancestry, only: [:edit]
 
   def index
-    @ladys = Product.where(category_id:1..190).first(10)
-    @mens = Product.where(category_id:200..345).first(10)
-    @appliances = Product.where(category_id:898..983).first(10)
-    @toys = Product.where(category_id:685..797).first(10)
+    @ladys = Product.where(category_id:1..190).order(id: "DESC").first(10)
+    @mens = Product.where(category_id:200..345).order(id: "DESC").first(10)
+    @appliances = Product.where(category_id:898..983).order(id: "DESC").first(10)
+    @toys = Product.where(category_id:685..797).order(id: "DESC").first(10)
 
-    @chanels = Product.where(brand_id:1).first(10)
-    @coachs = Product.where(brand_id:2).first(10)
-    @louisvuittons = Product.where(brand_id:3).first(10)
-    @guccis = Product.where(brand_id:4).first(10)
+    @chanels = Product.where(brand_id:1).order(id: "DESC").first(10)
+    @coachs = Product.where(brand_id:2).order(id: "DESC").first(10)
+    @louisvuittons = Product.where(brand_id:3).order(id: "DESC").first(10)
+    @guccis = Product.where(brand_id:4).order(id: "DESC").first(10)
   end
 
   def new
